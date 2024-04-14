e381289af71b05abe69b26f544894a032121deb1
Utsav-Kasvala <utsavkasvala52@gmail.com>
2024-04-14 14:25:55 +0530

    Making Initial Commit and Added reusable tailwind classes

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,80 @@
+import { useEffect,useRef } from 'react'
+import React from 'react'
+import logo from '../../assets/images/logo.png'
+import userimage from '../../assets/images/avatar-icon.png'
+import {NavLink , Link } from 'react-router-dom' 
+
+import  {BiMenu} from "react-icons/bi"
+const navLinks=[
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'/doctors',
+    display:' Find a Doctor'
+  },
+  {
+    path:'/services',
+    display:'Services'
+  },
+  {
+    path:'/contact',
+    display:'Contact'
+  },
+]
+
+const Header = () => {
+
+  
+
+  return (
+    <header className="header flex items-center" >
+      <div className="container">
+         <div className="flex items-center justify-between">
+           {/* -------LOGO HERE---------- */}
+            <div>
+              <img src={logo} alt="" />
+            </div>
+            {/* --------------Menu----------- */}
+            <div className="navigation" >
+            <ul className="menu flex items-center gap-[2.7rem]">
+            {navLinks.map((link, index) => (
+            <li key={index}>
+            <NavLink
+            to={link.path}
+          className={link.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'}>
+          {link.display}
+            </NavLink>
+          </li>
+          ))}
+        </ul>
+        </div>
+        
+        {/*----------- nav right---------------- */}
+         <div className="flex items-center gap-4">
+              <div className='hidden'>
+                <Link to='/'>
+                  <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                    <img src={userimage} className='w-full rounded-full' alt="" />
+                  </figure>
+                </Link>
+              </div>
+         
+         <Link to='/login'>
+          <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'> Login</button>
+         </Link>
+         
+         <span className='md:hidden' > 
+          <BiMenu className='w-6 h-6 cursor-pointer'/>
+         </span>
+         
+         </div>
+
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
