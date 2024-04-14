1e61a5a5d0463fb917b8f3f385ff3951ce9f718f
nirmalsolanki30 <solanki.nirmal847@gmail.com>
2024-04-14 19:23:22 +0530

    Worked on sticky header, toggle menu and somewhat of home page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -26,10 +26,31 @@ const navLinks=[
 
 const Header = () => {
 
-  
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const handleStickyHeader = ()=>{
+    window.addEventListener('scroll', ()=>{
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        headerRef.current.classList.add('sticky_header')
+      }
+      else
+      {
+        headerRef.current.classList.remove('sticky_header')
+      }
+    })
+  }
+
+  useEffect(()=>{
+    handleStickyHeader()
+
+    return()=> window.removeEventListener('scroll', handleStickyHeader)
+  })
+
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show_menu')
 
   return (
-    <header className="header flex items-center" >
+    <header className="header flex items-center"  ref={headerRef}>
       <div className="container">
          <div className="flex items-center justify-between">
            {/* -------LOGO HERE---------- */}
@@ -37,7 +58,7 @@ const Header = () => {
               <img src={logo} alt="" />
             </div>
             {/* --------------Menu----------- */}
-            <div className="navigation" >
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
             {navLinks.map((link, index) => (
             <li key={index}>
@@ -65,7 +86,7 @@ const Header = () => {
           <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'> Login</button>
          </Link>
          
-         <span className='md:hidden' > 
+         <span className='md:hidden' onClick={toggleMenu}> 
           <BiMenu className='w-6 h-6 cursor-pointer'/>
          </span>
          
