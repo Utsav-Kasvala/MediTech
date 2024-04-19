4e516b3c3ead771f2c31a0b0f639c9f5d0387e7f
nirmalsolanki30 <solanki.nirmal847@gmail.com>
2024-04-20 02:40:18 +0530

    Working on doctor profile

--- frontend/src/Dashboard/doctor-account/Profile.jsx
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {AiOutlineDelete} from 'react-icons/ai'
 
 const Profile = () => {
 
@@ -6,11 +7,17 @@ const [formData,setFormData]=useState({
     name:"",
     email:"",
     phone:"",
-    bio:""
+    bio:"",
+    gender:"",
+    specialization:"",
+    ticketPrice: 0,
+    qualifications:[{startingDate: "", endingDate: "", degree:"", university:""}],
+    experiences:[],
+    timeSlots:[]
 })
 const handleInputChange=e=>{
-
-}
+    setFormData({...formData, [e.target.name]:e.target.value})
+};
   return (
     <div>
         <h2 className='text-headingColor font-bold text-[24px]'>Profile Information</h2>
@@ -31,6 +38,68 @@ const handleInputChange=e=>{
             <div className='mb-5'>
                 <p className='form_label'>Bio*</p>
                 <input type="text" name="bio" value={formData.bio} onChange={handleInputChange} placeholder='Bio' className='form_input' maxLength={100}/>
+            </div>
+
+            <div className="mb-5">
+                <div className="grid grid-cols-3 gap-5 mb-[30px]">
+                    <div>
+                        <p className="form_label">Gender*</p>
+                        <select name="gender" value={formData.gender} onChange={handleInputChange}
+                        className='form_input py-3.5'>
+                            <option value="">Select</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </div>
+                    <div>
+                        <p className="form_label">Specialization*</p>
+                        <select name="specialization" value={formData.specialization} onChange={handleInputChange}
+                        className='form_input py-3.5'>
+                            <option value="">Select</option>
+                            <option value="surgeon">Surgeon</option>
+                            <option value="neurologist">Neurologist</option>
+                            <option value="dermatologist">Dermatologist</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <p className='form_label'>Ticket Price*</p>
+                        <input type="number" placeholder='100' name='ticketPrice' value={formData.ticketPrice} className='form_input' onChange={handleInputChange} />
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div className="mb-5">
+                <p className='form_label'>Qulaifications*</p>
+                {formData.qualifications?.map((item,index)=><div key={index}>
+                    <div>
+                        <div className='grid grid-cols-2 gap-5'>
+                            <div>
+                                <p className="form_label">Starting Date*</p>
+                                <input type="date" name='startingDate' value={item.startingDate} className='form_input' />
+                            </div>
+                            <div>
+                                <p className="form_label">Ending Date*</p>
+                                <input type="date" name='endingDate' value={item.endingDate} className='form_input' />
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-2 gap-5'>
+                            <div>
+                                <p className="form_label">Degree*</p>
+                                <input type="text" name='degree' value={item.degree} className='form_input' />
+                            </div>
+                            <div>
+                                <p className="form_label">University*</p>
+                                <input type="text" name='university' value={item.university} className='form_input' />
+                            </div>
+                        </div>
+
+                        <button><AiOutlineDelete/></button>
+                    </div>
+                </div>)}
             </div>
         </form>
     </div>
